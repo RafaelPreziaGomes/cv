d0fad042f582806745e038d2649474cfbbdb7d82
RafaelPreziaGomes <rafafprezia@gmail.com>
2022-07-11 17:56:04 -0400

    sended statci files

--- app.js
+++ app.js
@@ -2,30 +2,40 @@
 
 const express = require("express");
 
-const ejs = require("ejs");
-
 const fs = require("fs");
 
 // initialize express
 
-const app = express()
+const app = express();
 
 //  read the html files
 
-// read index.html
-
-const indexHtml = fs.readFileSync(__dirname, "./public/index.html")
+//  get the index.html file at route /
 
 // serve static files
+app.use(express.static("public"));
 
-app.get("/", (req,res) => {
-    
-    res.render(indexHtml)
-})
+app.get("/", (req, res) => {});
+
+//  get the html-website.html file at route /html-website
+
+app.get("/html-website", (req, res) => {
+  // set headers
+  res.setHeader("Content-Type", "text/html");
+  // send the Html file to the browser
+  res.sendFile(__dirname + "/public/html-website.html");
+});
+
+//  get portifolio-hub.html file at route /portifolio-hub
+app.get("/portifolio-hub", (req, res) => {
+  // set headers
+  res.setHeader("Content-Type", "text/html");
+  // serve static files
+  res.sendFile(__dirname + "/public/portifolio-hub.html");
+});
 
 //  listening for requests on port 3000
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
-
+  console.log("Server is running on port 3000");
+});
